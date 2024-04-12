8497cf737f1cd13abf773d9798e3b4f0408e8294
Christian Alares <christianalares@users.noreply.github.com>
2024-04-12 16:51:38 +0200

    Ignore supabase error (hack)

--- apps/dashboard/src/app/[locale]/(app)/layout.tsx
+++ apps/dashboard/src/app/[locale]/(app)/layout.tsx
@@ -1,5 +1,33 @@
 import { createClient } from "@midday/supabase/server";
 
+const conWarn = console.warn
+const conLog = console.log
+
+const IGNORE_WARNINGS = [
+  'Using supabase.auth.getSession() is potentially insecure',
+  'Using the user object as returned from supabase.auth.getSession()',
+]
+
+// biome-ignore lint/complexity/useArrowFunction: <explanation>
+console.warn = function (...args) {
+  const match = args.find((arg) =>
+    typeof arg === 'string' ? IGNORE_WARNINGS.find((warning) => arg.includes(warning)) : false,
+  )
+  if (!match) {
+    conWarn(...args)
+  }
+}
+
+// biome-ignore lint/complexity/useArrowFunction: <explanation>
+console.log = function (...args) {
+  const match = args.find((arg) =>
+    typeof arg === 'string' ? IGNORE_WARNINGS.find((warning) => arg.includes(warning)) : false,
+  )
+  if (!match) {
+    conLog(...args)
+  }
+}
+
 export default async function Layout({
   dashboard,
   login,
